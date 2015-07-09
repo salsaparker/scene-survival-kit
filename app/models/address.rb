d9ec058c19ebcde012890a8798a4987a41108429
Doyi Lee <doyilee@hotmail.com>
2015-07-09 16:34:19 -0600

    updated Address  model associations

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,6 @@
 class Address < ActiveRecord::Base
 	belongs_to :venue
+	belongs_to :venue_rep
+	belongs_to :visual_artist
+	belongs_to :musician
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,5 +2,4 @@ class Venue < ActiveRecord::Base
 	has_many :addresses
 	belongs_to :event
 	belongs_to :venue_rep
-	
 end
