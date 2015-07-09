6be229627eddd15e797cd1ba6f2be0beb8570446
Doyi Lee <doyilee@hotmail.com>
2015-07-09 16:01:21 -0600

    recreated and updated profile, visual artist, venuerep, musician models and migrations

--- db/migrate/20150709215853_create_profiles.rb
+++ db/migrate/20150709215853_create_profiles.rb
@@ -1,7 +1,7 @@
 class CreateProfiles < ActiveRecord::Migration
   def change
     create_table :profiles do |t|
-    	t.string :name
+			t.string :name
     	t.string :bio
     	t.string :phone_number
     	t.integer :user_id
--- db/migrate/20150709215914_create_venue_reps.rb
+++ db/migrate/20150709215914_create_venue_reps.rb
@@ -1,7 +1,7 @@
 class CreateVenueReps < ActiveRecord::Migration
   def change
     create_table :venue_reps do |t|
-      t.integer :profile_id
+			t.integer :profile_id
       t.timestamps null: false
     end
   end
