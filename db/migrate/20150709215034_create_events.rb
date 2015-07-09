e8e3ff5e3c93219ec379b6c2ab1bf2eb3ea17de2
reecebisel <reecebisel@gmail.com>
2015-07-09 15:57:01 -0600

    inital events model creation

---
+++ db/migrate/20150709215034_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :event_name
+      t.text :event_desc
+      t.datetime :event_start
+      t.integer :user_id
+      t.integer :venue_id
+      t.timestamps null: false
+    end
+  end
+end
