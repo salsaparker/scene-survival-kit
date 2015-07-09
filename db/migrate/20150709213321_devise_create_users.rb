b1d5c359a53cb23f11457a55235ae1b71f04182a
Tyler Parker <tparker42d2@gmail.com>
2015-07-09 16:05:22 -0600

    generated views for devise and an add table migration file to add full_name to

--- db/migrate/20150709213321_devise_create_users.rb
+++ db/migrate/20150709213321_devise_create_users.rb
@@ -2,8 +2,6 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
 
-      t.string :full_name,          null: false, default: ""
-
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -37,7 +35,6 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps null: false
     end
 
-    has_one :profile
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
