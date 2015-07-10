26f240a9f3694e9c4431afa436f155496cbb35b7
Tiffany Forsyth <tiffanyforsyth1@gmail.com>
2015-07-09 20:34:19 -0600

    events controller and CRUD actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,29 +14,44 @@ before_action :find_event, only: [:edit, :update, :show, :destroy]
   end
 
 	def create
-
+		@event = Event.new(event_params)
+		if @event.save
+			flash[:notice]= "Event created!"
+			redirect_to events_path
+		else
+			flash[:error]= "Something went wrong. Please try again."
+			render :new
+		end
 	end
 
   def edit
 
   end
 
-	def update
-
+def update
+		if @event.update(event_params)
+			flash[:notice]= "Event updated!"
+			redirect_to events_path
+		else
+			flash[:error]= "Something went wrong. Please try again."
+			render :edit
+		end
 	end
 
 	def destroy
-
+    @event.destroy
+		redirect_to events_path
 	end
 
 private
 
 	def find_event
-
+    @event = Event.find(params[:id])
 	end
 
 	def event_params
-
+		params.require(:event).permit(:event_name, :event_desc, :event_start,
+		:user_id, :venue_id, :created_at, :updated_at)
 	end
 
 end
