effdae114f40c2fac91054927791d7d58317318e
Tiffany Forsyth <tiffanyforsyth1@gmail.com>
2015-07-12 20:02:21 -0600

    got paperclip gem working YAY

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709222544) do
+ActiveRecord::Schema.define(version: 20150710222405) do
 
   create_table "addresses", force: :cascade do |t|
     t.float    "latitude"
@@ -51,8 +51,12 @@ ActiveRecord::Schema.define(version: 20150709222544) do
     t.string   "bio"
     t.string   "phone_number"
     t.integer  "user_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
