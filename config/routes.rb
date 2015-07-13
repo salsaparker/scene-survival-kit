e5145ff46222a52974fd436f50c9ecd78f8ea908
Tyler Parker <tparker42d2@gmail.com>
2015-07-13 17:38:59 -0600

    fixing a few devise things
    Change-Id: I5a74ff4a81bc3d9f147e299eee2c6d8ede19c285

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
- devise_for :users
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,8 +53,8 @@ Rails.application.routes.draw do
 
   # get 'addresses/edit'
 
- 
-  
+
+
 
   resources :profile
   # Example of regular route:
