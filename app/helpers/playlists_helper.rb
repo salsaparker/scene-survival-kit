90e458f05ba2f6052e0c2968b74f95fb33fe8a56
reecebisel <reecebisel@gmail.com>
2015-07-15 15:38:59 -0600

    logic added to playlist show page to embed both kinds of playlists
    Change-Id: Ic49037176749d3d40aff2967d2a9990fa14aee02

--- app/helpers/playlists_helper.rb
+++ app/helpers/playlists_helper.rb
@@ -1,18 +1,18 @@
 module PlaylistsHelper
 
-  def embed_player_url(url)
+  def embed_yt_url(url)
     # YouTube iframes expect urls in this format: https://www.youtube.com/embed/youtube_id
     # adapted from http://stackoverflow.com/questions/6556559/youtube-api-extract-video-id:
-    if player_match = url.to_s.match(/youtube.com.*(?:\/|v=)([^&$]+)/)
-      yt_uid = player_match[1]
-      yt_url = 'https://www.youtube.com/embed/' + yt_uid
-    end
-    if player_match = url.to_s.match(/soundcloud.com.*(?:\/|v=)/)
-      sc_playlist = url
-      sc_url = 'https://w.soundcloud.com/player/?url=' + sc_playlist
-      # binding.pry
-    end
+    player_match = url.to_s.match(/youtube.com.*(?:\/|v=)([^&$]+)/)
+    yt_uid = player_match[1]
+    yt_url = 'https://www.youtube.com/embed/' + yt_uid
+    # binding.pry
+  end
+ 
+  def embed_sc_url(url)
+    url.to_s.match(/soundcloud.com.*(?:\/|v=)/)
+    sc_playlist = url
+    sc_url = 'https://w.soundcloud.com/player/?url=' + sc_playlist
+    # binding.pry
   end
 end
-
-# <iframe id="sc-widget" src="<%= embed%>" width="100%" height="465" scrolling="no" frameborder="no"></iframe>
