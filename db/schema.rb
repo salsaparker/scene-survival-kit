893b46197fc337d17bbb14e59bbd4a1bbbe8535c
reecebisel <reecebisel@gmail.com>
2015-07-16 14:38:28 -0600

    added photo model with paperclip stuff
    Change-Id: Ica0c27440e33693b599f777e6b52670307362412

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714222721) do
+ActiveRecord::Schema.define(version: 20150716203519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,12 +54,29 @@ ActiveRecord::Schema.define(version: 20150714222721) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "photos", force: :cascade do |t|
+    t.string   "caption"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
+
   create_table "playlists", force: :cascade do |t|
     t.string   "playlist_url", null: false
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.integer  "profile_id"
     t.integer  "user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "body"
+    t.string   "body_html"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -73,6 +90,8 @@ ActiveRecord::Schema.define(version: 20150714222721) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "body"
+    t.string   "body_html"
   end
 
   create_table "users", force: :cascade do |t|
