2d49118a1640cbf535b5a4fbedb1a17f6265a785
reecebisel <reecebisel@gmail.com>
2015-07-17 14:54:11 -0600

    added relationships between models included dependencies
    Change-Id: I8fb21bbb96f7824c5b1f074af5ae423c6b022333

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,5 @@ class Event < ActiveRecord::Base
                                  # crops square at exactly ^ 150px
 																				 :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  hase_one :address, dependent: :destroy
 end
--- app/models/musician.rb
+++ app/models/musician.rb
@@ -1,3 +1,6 @@
 class Musician < ActiveRecord::Base
 	belongs_to :profile
+  has_many :playlists, dependent: :destroy
+  has_many :photos, dependent: :destroy
+  has_many :albums, dependent: :destroy
 end
