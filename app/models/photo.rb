3fc01b61ad3724ee3aebbf39314ee803df0f245a
reecebisel <reecebisel@gmail.com>
2015-07-17 14:47:34 -0600

    relationship to different profiles added
    Change-Id: I33c89a780c714660b09b91d8c933937f7c8de31c

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,7 @@ class Photo < ActiveRecord::Base
   has_attached_file :image, :styles => { :large => "600x600>" :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   belongs_to :profile
+  belongs_to :musician
+  belongs_to :venue_rep
+  belongs_to :visual_artist
 end
