773902355b523b6f22b73e70dca2dc370e03cd03
reecebisel <reecebisel@gmail.com>
2015-07-18 23:10:39 -0600

    added addres model spec
    Change-Id: Ibaead12cdc92aa18f418df9e8ef1b456c97ac339

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,5 +6,7 @@ class Event < ActiveRecord::Base
                                  # crops square at exactly ^ 150px
 																				 :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-  hase_one :address, dependent: :destroy
+  has_one :address, dependent: :destroy
+  belongs_to :user
+  belongs_to :venue
 end
