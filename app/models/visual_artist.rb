1c1cb648e594f71da8cbc3d2d575c2c7ad8bbb9e
Doyi Lee <doyilee@hotmail.com>
2015-07-19 15:42:52 -0600

    added association has many address for visual artist model
    Change-Id: Ie2baf4601a1ee46fda87efeb0129ebad594bc927

--- app/models/visual_artist.rb
+++ app/models/visual_artist.rb
@@ -3,5 +3,6 @@ class VisualArtist < ActiveRecord::Base
   has_many :photos, dependent: :destroy
   has_many :albums, dependent: :destroy
   has_many :playlists, dependent: :destroy
+  has_many :addresses
   accepts_nested_attributes_for :address, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
 end
