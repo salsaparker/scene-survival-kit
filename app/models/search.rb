8adc87d77045f4d77f69f06754738deb8c72fa6b
Doyi Lee <doyilee@hotmail.com>
2015-07-20 16:42:40 -0600

    changed associations to addresses not address in musicians model
    Change-Id: If55e02f167161a513408696b5d471bd90ef62827

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,13 @@
 class Search < ActiveRecord::Base
+	def search_profile
+		profiles = Profile.all
+		profile = profiles.where(["name LIKE ?", "%#{keywords}%"]) if keywords.present?
+
+	end
+end
+
+
+def search_address
+	musician = Musician.joins(:addresses).where(addresses: {"zip LIKE ?", "%#{zip}%"}) if zip.present?
+
 end
